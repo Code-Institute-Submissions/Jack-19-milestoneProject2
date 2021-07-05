99ba3384d93eea21b65a2f3c3d6c7e0089fd8ef1
Jack-19 <total90-@hotmail.com>
2021-07-05 18:17:27 +0000

    google map size chnaged to fit screen centrally and map title added centrally

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(51.501477,-0.058456),
+  zoom:13,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
 
