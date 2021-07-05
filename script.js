2c3a650ae7293ffa927569d2c8efb05689cec8f7
Jack-19 <total90-@hotmail.com>
2021-07-05 19:13:50 +0000

    javascript code started to add markers and marker clusters for sight locatins on google maps

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ var mapProp= {
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
 
+var label = "ABCDEFGHIJKLMNOP";
+
+var locations = [
+    {lat:51.497309, lng:-0.036993},
+    {lat:51.503133, lng:-0.003691},
+    {lat:51.482900, lng:-0.009763},
+];
+
+const markers = locations.map((location, i) => {
+    return new myMap.maps.marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+
+var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
