6eb93a9977f2d85057ef6eb388fbc819956cbd25
Jack-19 <total90-@hotmail.com>
2021-07-07 17:52:01 +0000

    google maps api edited

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 
+
+
 function myMap() {
 var mapProp= {
-  center:new google.maps.LatLng(51.501477,-0.058456),
-  zoom:13,
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
 };
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-};
-
-var label = "ABCDEFGHIJKLMNOP";
-
-var locations = [
-    {lat:51.497309, lng:-0.036993},
-    {lat:51.503133, lng:-0.003691},
-    {lat:51.482900, lng:-0.009763},
-];
-
-const markers = locations.map((location, i) => {
-    return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length],
-    });
-  });
-
-var markerCluster = new MarkerClusterer(map, markers, {
-    imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-  });
-
-
-
-  if(document.getElementById('lndBr').checked) {
-    return 
-
+}
