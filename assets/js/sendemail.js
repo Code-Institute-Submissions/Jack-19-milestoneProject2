3f901af69c1932d4c7c61099dba08906aafa403e
Jack-19 <total90-@hotmail.com>
2021-07-23 11:49:31 +0000

    email function added to contact form

---
+++ assets/js/sendemail.js
@@ -0,0 +1,20 @@
+
+
+function sendMail(contactForm) {
+    emailjs.send("service_sxkwv68","template_lwjzstp", {
+        "from_name": contactForm.fname.value,
+        "second_name":contactForm.lname.value,
+        "enquiry_type":contactForm.enquiry.value,
+        "from_email": contactForm.email.value,
+        "sight_request": contactForm.message.value,
+   })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
